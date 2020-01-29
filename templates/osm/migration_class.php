03da6b0559407d0fac0d568c617dab8e18a7b272
Vladislav Osmianskij <vlaosm@gmail.com>
2020-01-29 23:44:48 +0200

    `osm create:migration` command

---
+++ templates/osm/migration_class.php
@@ -0,0 +1,19 @@
+<?php
+/* @var string $namespace */
+/* @var string $class */
+?>
+<?php echo '<?php' ?>
+
+
+namespace <?php echo $namespace ?>;
+
+use Osm\Framework\Migrations\Migration;
+
+class <?php echo $class ?> extends Migration
+{
+    public function up() {
+    }
+
+    public function down() {
+    }
+}
